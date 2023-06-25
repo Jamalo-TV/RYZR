a9beb0a09931e0d95dfdbeb8ab93a49cdd58a02f
Gabriel Haefliger <gabriel.haefliger07@gmail.com>
2023-06-25 03:34:33 +0200

    Fixed Password check, fully working operational.

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def create_database():
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         email TEXT NOT NULL UNIQUE,
-        username TEXT NOT NULL UNIQUE,
         hashed_password TEXT NOT NULL,
         salt TEXT NOT NULL,
         generated_key NOT NULL,
@@ -37,19 +36,19 @@ def create_database():
     conn.close()
 
 
-def insert_user(email, username, hashed_password, salt, generated_key):
+def insert_user(email, hashed_password, salt, generated_key):
     conn = sqlite3.connect('register_base_cookie.db')
     cursor = conn.cursor()
 
     cursor.execute("""
-    INSERT INTO users (email, username, hashed_password, salt, generated_key) VALUES (?, ?, ?, ?, ?)
-    """, (email, username, hashed_password, salt, generated_key))
+    INSERT INTO users (email, hashed_password, salt, generated_key) VALUES (?, ?, ?, ?)
+    """, (email, hashed_password, salt, generated_key))
 
     conn.commit()
     conn.close()
 
 
-def register_user(username, password):
+def register_user(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password, salt
@@ -61,14 +60,20 @@ def validate_email(email):
     return pattern.match(email)
 
 
-def validate_username(username):
-    pattern = re.compile(r'^[\w]{3,30}$')
-    return pattern.match(username)
+#def validate_username(username):
+#    pattern = re.compile(r'^[\w]{3,30}$')
+#    return pattern.match(username)
 
 
 def validate_password(password):
     pattern = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$')
     return pattern.match(password)
+
+def compare_password(password, validate_password):
+    if password == validate_password:
+        return True
+    else:
+        return False
 
 
 # Flask routes
@@ -80,15 +85,15 @@ def index():
 @app.route('/input', methods=['POST'])
 def input_data():
     email = request.json['email']
-    username = request.json['username']
+    repeated_password = request.json['validate_password']
     password = request.json['password']
 
     if not validate_email(email):
         return jsonify(error="Invalid email format.")
 
-    if not validate_username(username):
+    if not compare_password(password, repeated_password):
         return jsonify(
-            error="Invalid username format. Must be 3-30 characters long and contain only alphanumeric characters and underscores.")
+            error="Passwords do not match.")
 
     if not validate_password(password):
         return jsonify(error="Invalid password format. Does not meet requirements.")
@@ -96,7 +101,7 @@ def input_data():
     conn = sqlite3.connect('register_base_cookie.db')
     cursor = conn.cursor()
 
-    hashed_password, salt = register_user(username, password)
+    hashed_password, salt = register_user(password)
 
     generated_key = key_generator()
 
@@ -104,24 +109,15 @@ def input_data():
 
     found_email = "You already have an account."
 
-    found_username = "Username has already been taken."
-
     cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
     result = cursor.fetchone()
     if result:
         print("Email has been found in the database")
 
         return jsonify(found_email=found_email)
+
     else:
-        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-        result = cursor.fetchone()
-        if result:
-            print("Username has been found in the database")
-
-            return jsonify(found_username=found_username)
-
-        else:
-            insert_user(email, username, hashed_password.decode('utf-8'), salt.decode('utf-8'), generated_key)
+            insert_user(email, hashed_password.decode('utf-8'), salt.decode('utf-8'), generated_key)
             print("User registered successfully!")
 
             return jsonify(validation_for_user=validation_for_user, generated_key=generated_key)
